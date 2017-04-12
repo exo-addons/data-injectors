9ae3c2cd7afa4c37d960dc92e4ef709aec48ecd3
Ameni Cherif <acherif@exoplatform.lan>
2017-04-12 17:53:35 +0100

    update plf version

--- services/src/main/java/org/exoplatform/injector/services/task/TaskInjector.java
+++ services/src/main/java/org/exoplatform/injector/services/task/TaskInjector.java
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU Lesser General Public License
 * along with this program. If not, see http://www.gnu.org/licenses/ .
 */
-package org.exoplatform.injector.services;
+package org.exoplatform.injector.services.task;
 
 import org.exoplatform.commons.api.persistence.ExoTransactional;
 import org.exoplatform.container.PortalContainer;
