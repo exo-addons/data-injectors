ac8a3f8ece0e141ef15db63e055895b00e1e0782
Romain Dénarié <romain.denarie@exoplatform.com>
2017-11-13 16:17:27 +0100

    change identityInjector to use random name and firstname

--- services/src/main/java/org/exoplatform/services/injection/social/IdentityInjector.java
+++ services/src/main/java/org/exoplatform/services/injection/social/IdentityInjector.java
@@ -2,6 +2,7 @@ package org.exoplatform.services.injection.social;
 
 import org.exoplatform.services.organization.User;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.fluttercode.datafactory.impl.DataFactory;
 
 import java.util.HashMap;
 
@@ -13,9 +14,12 @@ public class IdentityInjector extends AbstractSocialInjector {
   /** . */
   private static final String NUMBER = "number";
   private static final String PREFIX = "prefix";
+
+  private DataFactory dataFactory;
   
   public IdentityInjector(PatternInjectorConfig pattern) {
     super(pattern);
+    dataFactory = new DataFactory();
   }
   
   @Override
@@ -23,35 +27,42 @@ public class IdentityInjector extends AbstractSocialInjector {
 
     //
     int number = param(params, NUMBER);
-    String prefix = params.get(PREFIX);
-    init(prefix, null, userSuffixValue, spaceSuffixValue);
+//    String prefix = params.get(PREFIX);
+//    init(prefix, null, userSuffixValue, spaceSuffixValue);
 
     //
     for(int i = 0; i < number; ++i) {
 
       //
-      String username = this.userName();
-      User user = userHandler.createUserInstance(username);
-      user.setEmail(username + "@" + DOMAIN);
-      user.setFirstName(nameGenerator.compose(3));
-      user.setLastName(nameGenerator.compose(4));
-      user.setPassword(this.password);
 
-      try {
+      String firstname = dataFactory.getFirstName();
+      String lastName = dataFactory.getLastName();
+      String username = firstname.toLowerCase().charAt(0)+lastName.toLowerCase().replace("'","");
+      if (userHandler.findUserByName(username)==null) {
+        User user = userHandler.createUserInstance(username);
+        user.setEmail(username + "@" + DOMAIN);
+        user.setFirstName(firstname);
+        user.setLastName(lastName);
+        user.setPassword(this.password);
+
+        try {
+
+          //
+          userHandler.createUser(user, true);
+          identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, username, false);
+
+          //
+          ++userNumber;
+
+        } catch (Exception e) {
+          getLog().error(e);
+        }
 
         //
-        userHandler.createUser(user, true);
-        identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, username, false);
-
-        //
-        ++userNumber;
-
-      } catch (Exception e) {
-        getLog().error(e);
+        getLog().info("User " + username + " generated ("+(i+1)+"/"+number+")");
+      } else {
+        i--;
       }
-
-      //
-      getLog().info("User " + username + " generated");
 
     }
 
