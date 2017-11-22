153c795e0473a0728fd31a4f2559e532e02baed5
Romain Dénarié <romain.denarie@exoplatform.com>
2017-11-22 11:27:04 +0100

    update dataListing to get username by identities instead of userHnadler

--- services/src/main/java/org/exoplatform/services/listing/DataListingRESTService.java
+++ services/src/main/java/org/exoplatform/services/listing/DataListingRESTService.java
@@ -9,12 +9,17 @@ import org.exoplatform.services.organization.OrganizationService;
 import org.exoplatform.services.organization.User;
 import org.exoplatform.services.organization.UserHandler;
 import org.exoplatform.services.rest.resource.ResourceContainer;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.social.core.profile.ProfileFilter;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Created by Romain Dénarié (romain.denarie@exoplatform.com) on 10/11/17.
@@ -26,38 +31,43 @@ public class DataListingRESTService implements ResourceContainer {
 
     private PatternInjectorConfig patternInjectorConfig;
 
+    private IdentityManager identityManager;
+
     private static final Log LOG = ExoLogger.getLogger(DataListingRESTService.class);
 
 
     public DataListingRESTService(OrganizationService organizationService, InitParams params, PatternInjectorConfig
-            patternInjectorConfig) {
+            patternInjectorConfig, IdentityManager identityManager) {
         this.organizationService=organizationService;
         this.patternInjectorConfig = patternInjectorConfig;
+
+        this.identityManager = identityManager;
     }
 
     @GET
     @Path("/identities")
     @RolesAllowed({"administrators"})
     public Response identities() {
-        UserHandler userHandler=organizationService.getUserHandler();
+        long startTime = System.currentTimeMillis();
+
         String result = "";
-        int limit =100;
+        int limit =1000;
         int offset=0;
         try {
-            ListAccess<User> users = userHandler.findAllUsers();
-            int total = users.getSize();
+            ListAccess<Identity> identities = identityManager.getIdentitiesByProfileFilter(OrganizationIdentityProvider.NAME,new ProfileFilter(), false);
+            int total = identities.getSize();
             while (offset<total) {
 
-                User[] usersArray;
+                Identity[] identitiessArray;
                 if (total-offset<limit) {
                     //there is less than limit element to read. load to the end
-                    usersArray=users.load(offset, total-offset);
+                    identitiessArray=identities.load(offset, total-offset);
                 } else {
-                    usersArray=users.load(offset,limit);
+                    identitiessArray=identities.load(offset,limit);
                 }
-                for (User user:usersArray) {
-                    if (!user.getUserName().equals("root")) {
-                        result+=user.getUserName()+","+patternInjectorConfig.getUserPasswordValue()+"\n";
+                for (Identity identity:identitiessArray) {
+                    if (!identity.getRemoteId().equals("root")) {
+                        result+=identity.getRemoteId()+","+patternInjectorConfig.getUserPasswordValue()+"\n";
                     }
                 }
                 offset+=limit;
@@ -65,9 +75,12 @@ public class DataListingRESTService implements ResourceContainer {
         } catch (Exception e) {
             LOG.error("Error when getting user list",e);
         }
+        long endTime = System.currentTimeMillis();
+        LOG.info("Get all user with identities take {} ms.", (endTime-startTime));
+
 
         return Response.ok(result, MediaType.TEXT_PLAIN).build();
-
-
     }
+
 }
+
